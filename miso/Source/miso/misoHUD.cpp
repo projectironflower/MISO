7ac01180ded070554709415a559158e1c6f50e44
mogurinn <49937257+mogurinn@users.noreply.github.com>
2020-12-10 12:29:40 +0900

    警告が出るバグを直しました

--- miso/Source/miso/misoHUD.cpp
+++ miso/Source/miso/misoHUD.cpp
@@ -10,7 +10,7 @@
 AmisoHUD::AmisoHUD()
 {
 	// Set the crosshair texture
-	static ConstructorHelpers::FObjectFinder<UTexture2D> CrosshairTexObj(TEXT("/Game/FirstPerson/Textures/FirstPersonCrosshair"));
+	static ConstructorHelpers::FObjectFinder<UTexture2D> CrosshairTexObj(TEXT("/Game/Human/FirstPerson/Textures/FirstPersonCrosshair.FirstPersonCrosshair"));
 	CrosshairTex = CrosshairTexObj.Object;
 }
 
